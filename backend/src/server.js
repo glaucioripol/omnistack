71f8ea0935b71a9390e038300469946a086da933
Glaucio <glaucio.ricardo@tksolucoes.com.br>
2019-08-09 07:50:49 -0300

    connectando no banco

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const routes = require("./routes");
+
 const server = express();
+mongoose.connect("mongodb+srv://glaucioadmin:<6161@0792>@cluster0-vdz3h.mongodb.net/bdomnistack?retryWrites=true&w=majority",
+{
+    useNewUrlParser: true
+});
+
 
 server.use(express.json());
 server.use(routes);
