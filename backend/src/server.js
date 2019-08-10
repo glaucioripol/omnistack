09a8683d69ccbcc79a26347a24dfbb0ec4643608
Glaucio <glaucioripol@gmail.com>
2019-08-10 16:23:06 -0300

    updatees

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,8 +3,7 @@ const mongoose = require("mongoose");
 const routes = require("./routes");
 
 const server = express();
-mongoose.connect("mongodb+srv://glaucioadmin:6161@0792@cluster0-vdz3h.mongodb.net/bdomnistack?retryWrites=true&w=majority",
-{
+mongoose.connect("mongodb+srv://glaucioadmin:6161@0792@cluster0-vdz3h.mongodb.net/bdomnistack?retryWrites=true&w=majority",{
     useNewUrlParser: true
 });
 
