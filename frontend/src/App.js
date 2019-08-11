0930676187d900e35d9fcb27df4950e1b91a6e40
Glaucio <glaucioripol@gmail.com>
2019-08-10 22:37:01 -0300

    que daora isso vei

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from "./assets/logo.svg";
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          
+        <img src={logo} alt="tindex" title="tindev"/>
           <h2>Que delicia cara</h2>
+
         </div>
-        
+
       </div>
     );
   }
