cb70df38cf61bd2c5c0e584ad53e5174b940b49b
Glaucio Ricardo <glaucio.ricardo@tksolucoes.com.br>
2019-08-13 07:44:08 -0300

    muito delicia esse react véi

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 // esse match pega todos os parametros jogados nesta rota
-function Main({ match}) {
+function Main({ match }) {
     // pegando o paramentro que foi para rota match.params. o nome dele
     return (
         <h1>Hello world: {match.params.id}</h1>
